2b6d171f7898eefe4d1221c1da881c9f4a4172c9
Yesmani <yesmani.fa@gmail.com>
2024-05-14 16:44:21 -0400

    implementacion de configuracion inicial

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const express = require('express')
+/* const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hola Mundo')
 })
 
-app.listen(3000)
+app.listen(3000) */
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+//CONFIGURACION DEL SERVIDOR
+app.set('port', process.env.PORT || port);
+
+// Configuración de bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configuración de EJS como motor de plantillas
+app.set('view engine', 'ejs');
+
+// Uso de Middlewares
+app.use(morgan('dev'));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Bienvenido a al gestor de tareas!');
+  });
+
+  // Iniciar el servidor
+app.listen(app.get('port'), () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+  });
